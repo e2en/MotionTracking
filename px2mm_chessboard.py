b5c27bf82208b3cf6fd83499147817863361c163
Eren Aydemir <eren.aydemir@gmail.com>
2016-11-05 23:22:47 +0200

    px2mm correlation using HarrisCorner detection

--- px2mm_chessboard.py
+++ px2mm_chessboard.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 import glob
 import os
+import matplotlib.pyplot as plt
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = np.zeros((2*2,3), np.float32)
+objp[:,:2] = np.mgrid[0:2,0:2].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -35,33 +36,29 @@ for files in types:
     images.extend(glob.glob(files))
 
 for fname in images:
+
     img = cv2.imread(fname)
 
     h,  w = img.shape[:2]
     newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
-    # undistort
+    ## undistort
     dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 
     # crop the image
     x,y,w,h = roi
     dst = dst[y:y+h, x:x+w]
-    #cv2.imshow('undistorted', dst)
     
-    gray = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
+    imgROI=img[220:653, 218:680]
+    gray = cv2.cvtColor(imgROI,cv2.COLOR_BGR2GRAY)
     
-    ret, corners = cv2.findChessboardCorners(gray, (6,9),None)
+    #ret, corners = cv2.findChessboardCorners(gray, (2,2),None)
+    corners=cv2.cornerHarris(gray,2,3,0.04)
+    corners=cv2.dilate(corners,None)
     
-    if ret == True:
-        print (corners[0] - corners[1])
-        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        print fname
-        print (corners[0] - corners[1])
-        
-        for i in corners[1,:]:
-            # draw the center of the circle
-            cv2.circle(dst,(i[0],i[1]),2,(0,0,255),3)
-            cv2.imshow('corners', dst)
-            cv2.waitKey(1000)  
+    imgROI[corners>0.1*corners.max()]=[255,0,0]
+    #print imgpoints[1][2][0]-imgpoints[0][2][0]
+    plt.imshow(img)
+    plt.show()
                  
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
